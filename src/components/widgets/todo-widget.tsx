896053ff582d7483a8f942e1f8d0dad3627e7be1
simplyyliam <simplyliam.dev@gmail.com>
2026-01-08 21:28:54 +0200

    🎬animation(TodoWidget): add smooth entry animation to form element when handleShowInput is being clicked.

--- src/components/widgets/todo-widget.tsx
+++ src/components/widgets/todo-widget.tsx
@@ -8,7 +8,8 @@ import {
 import { Checkbox } from "../ui/checkbox";
 import { HugeiconsIcon } from "@hugeicons/react";
 import { Add01Icon } from "@hugeicons/core-free-icons";
-import { useState, type FormEvent } from "react";
+import { useRef, useState, useEffect, type FormEvent } from "react";
+import { gsap } from "gsap";
 
 type Todo = { id: number; text: string };
 
@@ -16,6 +17,8 @@ export default function TodoWidget() {
   const [showInput, setShowInput] = useState(false);
   const [todos, setTodos] = useState<Todo[]>([]);
   const [input, setInput] = useState("");
+
+  const formRef = useRef<HTMLFormElement | null>(null);
 
   function addTodo(e: FormEvent) {
     e.preventDefault();
@@ -34,6 +37,21 @@ export default function TodoWidget() {
     setShowInput((prev) => !prev);
   }
 
+  useEffect(() => {
+    if (showInput && formRef.current) {
+      gsap.fromTo(
+        formRef.current,
+        { scale: 0.5, autoAlpha: 0 },
+        {
+          scale: 1,
+          autoAlpha: 1,
+          duration: 0.6,
+          ease: "back.out",
+        }
+      );
+    }
+  }, [showInput]);
+
   return (
     <Card className="absolute left-0 top-5 w-full max-w-sm bg-white border-none shadow-none rounded-[30px]">
       <CardHeader>
@@ -47,6 +65,7 @@ export default function TodoWidget() {
           </button>
         </CardAction>
       </CardHeader>
+
       {todos.length === 0 && !showInput ? (
         <div className="flex items-center justify-center w-full h-full text-muted-foreground p-2.5">
           Add a todo
@@ -64,14 +83,20 @@ export default function TodoWidget() {
               </div>
             </div>
           ))}
+
           {showInput && (
-            <form onSubmit={addTodo} className="flex p-2.5 w-full h-fit">
+            <form
+              ref={formRef}
+              onSubmit={addTodo}
+              className="flex p-2.5 w-full h-fit"
+            >
               <input
                 className="w-full p-2.5"
                 type="text"
                 value={input}
                 onChange={(e) => setInput(e.target.value)}
                 placeholder="Todo..."
+                autoFocus
               />
             </form>
           )}
