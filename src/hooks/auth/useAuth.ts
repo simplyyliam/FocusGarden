504024929f235263f08ed91dfdd821fcb44d1814
simplyyliam <simplyliam.dev@gmail.com>
2026-01-12 12:27:05 +0200

    ✨ feat(auth): enhance useAuth hook with session management and sign-out functionality

--- src/hooks/auth/useAuth.ts
+++ src/hooks/auth/useAuth.ts
@@ -1,8 +1,30 @@
 import { supabase } from "@/lib"
+import type { Session } from "@supabase/supabase-js"
+import { useEffect, useState } from "react"
 
 
 
 export default function useAuth() {
+    const [session, setSession] = useState<Session | null>(null)
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        supabase.auth.getSession().then(({
+            data: { session }
+        }) => {
+            setSession(session)
+            setLoading(false)
+        })
+
+        const { data: { subscription } } =
+            supabase.auth.onAuthStateChange((_event, session) => {
+                setSession(session)
+            })
+
+        return () => subscription.unsubscribe()
+
+    }, [])
+
     const signInWithGoogle = async () => {
         await supabase.auth.signInWithOAuth({
             provider: "google",
@@ -12,7 +34,16 @@ export default function useAuth() {
         })
 
     }
-    // const logOut = () => {}
 
-    return { signInWithGoogle }
+    const signOut = async () => {
+        await supabase.auth.signOut()
+    }
+
+    return {
+        session,
+        signInWithGoogle,
+        user: session?.user,
+        loading,
+        signOut
+    }
 }
