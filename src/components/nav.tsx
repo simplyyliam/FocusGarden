ec3f3454bb9337f83fc4760e04d4ea0d21733774
simplyyliam <simplyliam.dev@gmail.com>
2026-01-14 21:15:28 +0200

    ✨ feat(Nav, Avatar): add user avatar functionality and create Avatar component for improved user experience

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,10 +1,48 @@
+import { supabase } from "@/lib";
 import { BarContainer } from "./shared";
+import { useEffect, useState } from "react";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 
 export default function Nav() {
+  const [name, setName] = useState("");
+  const [avatar, setAvatar] = useState();
+
+  useEffect(() => {
+    async function getUser() {
+      const {
+        data: { user },
+      } = await supabase.auth.getUser();
+      try {
+        if (user) {
+          setName(
+            user.user_metadata?.full_name ||
+              user.user_metadata?.name ||
+              user.email ||
+              ""
+          );
+          // console.log(user.user_metadata)
+          const avatarUrl =
+            user.user_metadata?.avatar_url || "No avatar was found";
+          setAvatar(avatarUrl);
+          // console.log("Avatar Image:", avatarUrl)
+          return user;
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    getUser();
+  }, []);
+
   return (
     <BarContainer>
-      <div className="w-13 h-13 rounded-2xl bg-white flex items-center justify-center">
-        P
+      <div className="flex items-center justify-center gap-3 text-white">
+        <Avatar>
+          <AvatarImage src={avatar} />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+        {name}
       </div>
     </BarContainer>
   );
