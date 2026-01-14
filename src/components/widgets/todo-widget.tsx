70eeef66522e54b4059373e3bd5d5d13cee63daf
simplyyliam <simplyliam.dev@gmail.com>
2026-01-14 17:52:21 +0200

    ✨ feat(TodoWidget): add context menu functionality to todo widget
    - Added @radix-ui/react-context-menu dependency to package.json.
    - Implemented context menu in TodoWidget for todo item actions (delete, billing, team, subscription).
    - Created a new ContextMenu component with necessary subcomponents for better UI handling.
    - Refactored todo item rendering to include context menu trigger and content.

--- src/components/widgets/todo-widget.tsx
+++ src/components/widgets/todo-widget.tsx
@@ -10,9 +10,15 @@ import {
 import { Checkbox } from "../ui/checkbox";
 import { HugeiconsIcon } from "@hugeicons/react";
 import { Add01Icon } from "@hugeicons/core-free-icons";
-import { useState, type FormEvent } from "react";
+import { useRef, useState, type FormEvent } from "react";
 
 import { AnimatePresence, motion } from "motion/react";
+import {
+  ContextMenu,
+  ContextMenuContent,
+  ContextMenuItem,
+  ContextMenuTrigger,
+} from "../ui/context-menu";
 
 type Todo = { id: number; text: string; completed: boolean };
 
@@ -21,6 +27,7 @@ export default function TodoWidget() {
   const [todos, setTodos] = useState<Todo[]>([]);
   const [input, setInput] = useState("");
 
+  const todoRef = useRef<HTMLButtonElement | null>(null);
   function addTodo(e: FormEvent) {
     e.preventDefault();
     if (!input.trim()) return;
@@ -30,94 +37,112 @@ export default function TodoWidget() {
     setShowInput(false);
   }
 
-  // function deleteTodo(id: number) {
-  //   setTodos((prev) => prev.filter((todo) => todo.id !== id));
-  // }
-
+  function deleteTodo(id: number) {
+    setTodos((prev) => prev.filter((todo) => todo.id !== id));
+  }
 
   return (
-    <Card className="absolute left-0 top-5 w-full max-w-sm bg-white border-none shadow-none rounded-[30px]">
-      <CardHeader>
-        <CardTitle>Tasks</CardTitle>
-        <CardAction>
-          <motion.button
-            whileTap={{ y: 1 }}
-            onClick={() => setShowInput(!showInput)}
-            className="flex items-center justify-center w-8.75 h-8.75 rounded-[10px] bg-[#F7F7F7]"
-          >
-            <HugeiconsIcon icon={Add01Icon} />
-          </motion.button>
-        </CardAction>
-      </CardHeader>
-
-      <CardContent>
-        <AnimatePresence mode="wait">
-          {todos.length === 0 && !showInput && (
-            <motion.div
-              key="empty"
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              exit={{ opacity: 0 }}
-              className="flex items-center justify-center w-full text-muted-foreground p-2.5"
+    <>
+      <Card className="absolute left-0 top-5 w-full max-w-sm bg-white border-none shadow-none rounded-[30px]">
+        <CardHeader>
+          <CardTitle>Tasks</CardTitle>
+          <CardAction>
+            <motion.button
+              whileTap={{ y: 1 }}
+              onClick={() => setShowInput(!showInput)}
+              className="flex items-center justify-center w-8.75 h-8.75 rounded-[10px] bg-[#F7F7F7]"
             >
-              Add a todo
-            </motion.div>
-          )}
+              <HugeiconsIcon icon={Add01Icon} />
+            </motion.button>
+          </CardAction>
+        </CardHeader>
 
-          {todos.map((t) => (
-            <div key={t.id} className="flex p-2.5 w-full">
+        <CardContent>
+          <AnimatePresence mode="wait">
+            {todos.length === 0 && !showInput && (
               <motion.div
-                initial={{ opacity: 0, scale: 0 }}
+                key="empty"
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+                className="flex items-center justify-center w-full text-muted-foreground p-2.5"
+              >
+                Add a todo
+              </motion.div>
+            )}
+
+            {todos.map((t) => (
+              <ContextMenu key={t.id}>
+                <ContextMenuTrigger asChild>
+                  <button
+                    ref={todoRef}
+                    key={t.id}
+                    className="flex p-2.5 w-full cursor-pointer rounded-xl hover:bg-accent transition-colors ease-linear "
+                  >
+                    <motion.div
+                      initial={{ opacity: 0, scale: 0 }}
+                      animate={{ opacity: 1, scale: 1 }}
+                      exit={{ opacity: 0, scale: 0.95 }}
+                      key={t.id}
+                      className="flex items-center gap-2.5"
+                    >
+                      <Checkbox
+                        id={t.id.toString()}
+                        checked={t.completed}
+                        onCheckedChange={() =>
+                          setTodos((prev) =>
+                            prev.map((todo) =>
+                              todo.id === t.id
+                                ? { ...todo, completed: !t.completed }
+                                : todo
+                            )
+                          )
+                        }
+                      />
+                      <label
+                        htmlFor={t.id.toString()}
+                        className={`${
+                          t.completed
+                            ? "line-through text-muted-foreground"
+                            : ""
+                        }`}
+                      >
+                        {t.text}
+                      </label>
+                    </motion.div>
+                  </button>
+                </ContextMenuTrigger>
+                <ContextMenuContent>
+                  <ContextMenuItem onClick={() => deleteTodo(t.id)}>Delete</ContextMenuItem>
+                  <ContextMenuItem>Billing</ContextMenuItem>
+                  <ContextMenuItem>Team</ContextMenuItem>
+                  <ContextMenuItem>Subscription</ContextMenuItem>
+                </ContextMenuContent>
+              </ContextMenu>
+            ))}
+
+            {showInput && (
+              <motion.div
+                key="input"
+                initial={{ opacity: 0, scale: 0.95 }}
                 animate={{ opacity: 1, scale: 1 }}
                 exit={{ opacity: 0, scale: 0.95 }}
-                key={t.id}
-                className="flex items-center gap-2.5"
               >
-                <Checkbox
-                  id={t.id.toString()}
-                  checked={t.completed}
-                  onCheckedChange={() =>
-                    setTodos((prev) =>
-                      prev.map((todo) =>
-                        todo.id === t.id
-                          ? { ...todo, completed: !t.completed }
-                          : todo
-                      )
-                      
-                    )
-                  }
-                />
-                <label
-                  htmlFor={t.id.toString()}
-                  className={`${t.completed ? "line-through text-muted-foreground" : ""}`}
-                >
-                  {t.text}
-                </label>
+                <form onSubmit={addTodo} className="flex p-2.5 w-full">
+                  <input
+                    className="w-full p-2.5"
+                    type="text"
+                    value={input}
+                    onChange={(e) => setInput(e.target.value)}
+                    placeholder="Todo..."
+                    autoFocus
+                  />
+                </form>
               </motion.div>
-            </div>
-          ))}
-
-          {showInput && (
-            <motion.div
-              key="input"
-              initial={{ opacity: 0, scale: 0.95 }}
-              animate={{ opacity: 1, scale: 1 }}
-              exit={{ opacity: 0, scale: 0.95 }}
-            >
-              <form onSubmit={addTodo} className="flex p-2.5 w-full">
-                <input
-                  className="w-full p-2.5"
-                  type="text"
-                  value={input}
-                  onChange={(e) => setInput(e.target.value)}
-                  placeholder="Todo..."
-                  autoFocus
-                />
-              </form>
-            </motion.div>
-          )}
-        </AnimatePresence>
-      </CardContent>
-    </Card>
+            )}
+          </AnimatePresence>
+        </CardContent>
+      </Card>
+    </>
   );
 }
