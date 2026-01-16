b064f41d2ea95306916990a8a1e55c0e94b7f98d
simplyyliam <simplyliam.dev@gmail.com>
2026-01-16 13:27:12 +0200

    ✨ feat(AuthContext): implement signOut function placeholder

--- src/auth/AuthContex.ts
+++ src/auth/AuthContex.ts
@@ -5,4 +5,7 @@ export const AuthContext = createContext<AuthProviderType>({
   session: null,
   user: null,
   isLoading: true,
+  signOut: function (): Promise<void> {
+    throw new Error("Function not implemented.");
+  }
 });
